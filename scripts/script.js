5704b885e36354f1d49baa0c86e9af7c8fe1d76f
Yaniv Schweitzer <yaniv10501@gmail.com>
2021-08-21 13:06:01 +0300

    Feat: All but responsive

---
+++ scripts/script.js
@@ -0,0 +1,76 @@
+
+let edit = document.querySelector(".profile__edit-rectangle");
+
+let popup = document.querySelector(".popup");
+
+let cls = document.querySelector(".popup__close-button");
+
+let formElement = document.querySelector(".popup__save-button");
+
+let profileName = document.querySelector(".profile__name");
+
+let profileAbout = document.querySelector(".profile__description");
+
+function openPopup() {
+
+  popup.classList.add("popup_opened");
+
+}
+
+function closePopup() {
+
+  popup.classList.remove("popup_opened");
+
+}
+
+function handleFormSubmit(evt) {
+
+  evt.preventDefault();
+
+  let nameInput = document.querySelector(".popup__name-input").value;
+
+  let jobInput = document.querySelector(".popup__about-input").value;
+
+  if (!nameInput && !jobInput) {
+
+    popup.classList.remove("popup_opened");
+
+  }
+
+  else if (nameInput && !jobInput) {
+
+    profileName.textContent = nameInput;
+
+    popup.classList.remove("popup_opened");
+
+  }
+
+  else if (!nameInput && jobInput) {
+
+    profileAbout.textContent = jobInput;
+
+    popup.classList.remove("popup_opened");
+
+  }
+
+  else {
+
+    profileName.textContent = nameInput;
+
+    profileAbout.textContent = jobInput;
+
+    popup.classList.remove("popup_opened");
+
+  }
+
+}
+
+edit.addEventListener("click", openPopup);
+
+cls.addEventListener("click", closePopup);
+
+formElement.addEventListener("click", handleFormSubmit);
+
+
+
+
